ceecddc434947107e1609974af4526aca1b48fae
Alan <holasoyalan9@gmail.com>
2024-10-17 19:31:05 -0600

    Agregué la funcion de la pendiente por dos puntos en nuestro archivo principal de funciones

--- Funciones/funcionesParaOperaciones.h
+++ Funciones/funcionesParaOperaciones.h
@@ -1,4 +1,5 @@
 void MenuLineaRecta(int opcion_lr);
 float EcuasionPendiente(int m, int x, int b);
+float RectaPorDosPuntos(int x1, int y1, int x2, int y2, int m, int b);
 
 // int FuncionPrueba(int a, int b);
