f69b9e371fefb9ca7e52cc0aa014d2e217d7c301
Alan <holasoyalan9@gmail.com>
2024-10-22 13:46:06 -0600

    Agregado el archivo y el menú de coordenadas polares

--- Menu_Coordenadas_Polares.c
+++ Menu_Coordenadas_Polares.c
@@ -3,10 +3,8 @@
 
 void MenuCoordenadasPolares()
 {
-  
   printf("-------COORDENADAS POLARES-------");
   printf("1.Pasar un puntode coordenadasrectangularesa polares");
   printf("2.Pasarun punto de coordenadas polares a rectangulares");
   printf("3.Regresar al menu principal");
-
 }
